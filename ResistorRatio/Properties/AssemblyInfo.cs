28505e034b63a6cf0d8dfdfa34765c7480d42c2f
EsseivaN <nicolas.esseiva@gmail.com>
2019-03-16 17:44:13 +0100

    update

--- ResistorRatio/Properties/AssemblyInfo.cs
+++ ResistorRatio/Properties/AssemblyInfo.cs
@@ -19,8 +19,8 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-
-
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3a24ec05-3a28-4a69-ab5d-000000000002")]
 
 // Version information for an assembly consists of the following four values:
 //
